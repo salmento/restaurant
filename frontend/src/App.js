7424fee2f96538534bdbb6e0b19c249470bfaa7d
salmento <salmentochitlango@gmail.com>
2021-05-04 10:16:01 +0200

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    Hello word.
+    </div>
+  );
+}
+
+export default App;
